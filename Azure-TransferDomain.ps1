9161e9b61d992df0fb0da14a3fad8438e6caf6c5
Wellington Ozorio <45944705+wozorio@users.noreply.github.com>
2020-12-30 11:05:18 -0300

    Update Azure-TransferDomain.ps1

--- Azure-TransferDomain.ps1
+++ Azure-TransferDomain.ps1
@@ -9,12 +9,12 @@
 #>
 
 # Change the value of the variables below according to your environment
-$resourceName = 'singlekey-id.com'
+$domainName = 'domain.com' # domain name
 $resourceLocation = 'Global'
+$resourceGroup = 'rg-prod-domain' # RG where the App Service Domain will be created
 $ipAddress = '192.168.1.1' # user's IP address
-$authCode = 'xpto123456' # code obtained from their current provider in order to transfer out the domain
+$authCode = 'xpto123456' # authorization code obtained from the current provider (registrar) in order to transfer out the domain
 $currentTime = '2020-12-30T13:55:30.6850404Z' # roughly the current time (2017-13-05T12:25:30.6850404Z)
-$resourceGroup = 'rg-prod-domain'
 
 $propertiesObject = @{
   'Consent'   = @{
@@ -29,7 +29,7 @@ $propertiesObject = @{
 
 # Create an App Service Domain and initiate the domain transfer
 New-AzureRmResource `
-  -ResourceName $resourceName `
+  -ResourceName $domainName `
   -Location $resourceLocation `
   -PropertyObject $propertiesObject `
   -ResourceGroupName $resourceGroup `
